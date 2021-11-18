a7a31cf0d33bd34a8ec138c608d721c0f7f3123e
Daniel Toplak <homer.simpson123@gmx.de>
2021-11-18 23:08:24 +0100

    Fix: add new extension to deps and show message to uninstall

--- src/extension.ts
+++ src/extension.ts
@@ -2,22 +2,18 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-import { GLSLLintingProvider } from './features/glsllintProvider';
-import { GLSLifyProvider } from './features/glslifyProvider';
-
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext): void {
-  const glslifyProvider = new GLSLifyProvider();
-  const providerRegistrations = vscode.Disposable.from(
-    vscode.workspace.registerTextDocumentContentProvider(GLSLifyProvider.scheme, glslifyProvider)
-  );
-
-  const linter = new GLSLLintingProvider();
-  linter.activate(context.subscriptions);
-  //vscode.languages.registerCodeActionsProvider('glsl', linter);
-
-  context.subscriptions.push(providerRegistrations);
+  vscode.window
+    .showInformationMessage(
+      'This extension is deprecated.\nIt can be removed without any problems.\nShould the extension be removed now?',
+      'Yes',
+      'No'
+    )
+    .then((answer) => {
+      vscode.commands.executeCommand('workbench.extensions.uninstallExtension', 'CADENAS.vscode-glsllint');
+    });
 }
 
 // this method is called when your extension is deactivated
